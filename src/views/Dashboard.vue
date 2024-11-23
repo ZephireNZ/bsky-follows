053e39cc7ac62a3165530875b9fe1b5e60e64e91
ZephireNZ <brynley+github@zephire.nz>
2024-11-23 15:18:32 +1300

    Only show top 20 profiles for now

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,23 +1,19 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed } from 'vue'
 import { useBskyCrawling } from '@/composables/bluesky'
 import BskyProfile from '../components/BskyProfile.vue'
-import { Slider } from 'primevue'
 
 const { ranked } = useBskyCrawling()
 
-const minFollows = ref(3)
+const shown = computed(() => ranked.value.filter((f) => f.rank <= 20))
 </script>
 
 <template>
   <main class="mt-12">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <Slider :step="1" :min="1" :max="20" v-model="minFollows" />
-      <p>Follows Shown: {{ minFollows }}</p>
-
       <ul role="list" class="divide-y divide-gray-100">
         <li
-          v-for="follow of ranked"
+          v-for="follow of shown"
           :key="follow.profile.did"
           class="flex justify-between gap-x-6 py-5"
         >
